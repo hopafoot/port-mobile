473fa01612860929c9ec081ce2386e2447ff7904
Abhinav <abhinav@mail.numberless.tech>
2025-06-25 15:53:05 -0400

    Add helper to compare std::vectors in gtest

---
+++ shared/tests/vectorcmp.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define ASSERT_VEC_EQ(x, y)                               \
+  do                                                      \
+  {                                                       \
+    ASSERT_EQ(x.size(), y.size());                        \
+    for (int i = 0; i < x.size(); i++)                    \
+    {                                                     \
+      EXPECT_EQ(x[i], y[i]) << "x and y differ at " << i; \
+    }                                                     \
+  } while (0)
