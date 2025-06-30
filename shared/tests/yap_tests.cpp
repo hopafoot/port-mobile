dbf3d89e3865a2bb42bfaea301f006308097a7c8
Abhinav <abhinav@mail.numberless.tech>
2025-06-30 15:35:32 -0400

    Add tests for expected failures in YAP

--- shared/tests/yap_tests.cpp
+++ shared/tests/yap_tests.cpp
@@ -4,11 +4,13 @@
 #include "commonrand.hpp"
 #include "x25519.hpp"
 #include "yap.hpp"
+#include "aesgcm.hpp"
 
 /**
  * End-to-end tests for encryption using YAP.
  */
 
+// Verbosely test the successful case with a short, human readable pplaintext
 TEST(YAPTests, E2EHumanReadable)
 {
   // Set up Alice and Bob
@@ -39,20 +41,15 @@ TEST(YAPTests, E2EHumanReadable)
   ASSERT_STREQ(human_readable_plaintext_decrypted_by_bob.c_str(), human_readable_plaintext.c_str());
 }
 
+// Test the success case for a random plaintext that's an odd size and at least 1k
 TEST(YAPTests, E2ERand)
 {
   // Set up Alice and Bob
   auto alice_keypair = x25519::generate_keypair();
   auto bob_keypair = x25519::generate_keypair();
-  // Assert that the public keys are the expected length
-  EXPECT_EQ(32, alice_keypair->public_key.size());
-  EXPECT_EQ(32, bob_keypair->public_key.size());
-  EXPECT_EQ(32, alice_keypair->private_key.size());
-  EXPECT_EQ(32, bob_keypair->private_key.size());
 
   // Check that the shared secrets are equal
   auto shared_secret = x25519::derive_secret(alice_keypair->private_key, bob_keypair->public_key);
-  ASSERT_VEC_EQ(shared_secret, x25519::derive_secret(bob_keypair->private_key, alice_keypair->public_key));
 
   // Randomized plaintext that's big and an odd size
   auto plaintext = encoders::hex_to_binary(commonrand::hex(1050));
@@ -63,4 +60,110 @@ TEST(YAPTests, E2ERand)
 
   // Assert that plaintexts match
   ASSERT_VEC_EQ(plaintext, plaintext_decrypted_by_bob);
+}
+
+/**
+ * Test failure cases below this point. Attempt to corrupt different parts and ensure that
+ * decryption fails and throws an exception.
+ */
+
+TEST(YAPTests, BadEphemeralPubkey)
+{
+  // Set up Alice and Bob
+  auto alice_keypair = x25519::generate_keypair();
+  auto bob_keypair = x25519::generate_keypair();
+
+  // Check that the shared secrets are equal
+  auto shared_secret = x25519::derive_secret(alice_keypair->private_key, bob_keypair->public_key);
+
+  // Randomized plaintext that's big and an odd size
+  auto plaintext = encoders::hex_to_binary(commonrand::hex(1050));
+
+  // Finally YAP away
+  auto ciphertext_from_alice = yap::v1::encrypt(shared_secret, bob_keypair->public_key, plaintext);
+  // Flip the first bit of the ephemeral public key
+  // Flip the first bit of the nonce
+  int pos_to_flip = 0;
+  ciphertext_from_alice[pos_to_flip] = 0x1 ^ ciphertext_from_alice[pos_to_flip];
+  ASSERT_ANY_THROW(yap::v1::decrypt(shared_secret, bob_keypair->private_key, ciphertext_from_alice));
+}
+
+TEST(YAPTests, BadNonce)
+{
+  // Set up Alice and Bob
+  auto alice_keypair = x25519::generate_keypair();
+  auto bob_keypair = x25519::generate_keypair();
+
+  // Check that the shared secrets are equal
+  auto shared_secret = x25519::derive_secret(alice_keypair->private_key, bob_keypair->public_key);
+
+  // Randomized plaintext that's big and an odd size
+  auto plaintext = encoders::hex_to_binary(commonrand::hex(1050));
+
+  // Finally YAP away
+  auto ciphertext_from_alice = yap::v1::encrypt(shared_secret, bob_keypair->public_key, plaintext);
+  // Flip the first bit of the nonce
+  int pos_to_flip = x25519::PUBLIC_KEY_LENGTH;
+  ciphertext_from_alice[pos_to_flip] = 0x1 ^ ciphertext_from_alice[pos_to_flip];
+  ASSERT_ANY_THROW(yap::v1::decrypt(shared_secret, bob_keypair->private_key, ciphertext_from_alice));
+}
+
+TEST(YAPTests, BadTag)
+{
+  // Set up Alice and Bob
+  auto alice_keypair = x25519::generate_keypair();
+  auto bob_keypair = x25519::generate_keypair();
+
+  // Check that the shared secrets are equal
+  auto shared_secret = x25519::derive_secret(alice_keypair->private_key, bob_keypair->public_key);
+
+  // Randomized plaintext that's big and an odd size
+  auto plaintext = encoders::hex_to_binary(commonrand::hex(1050));
+
+  // Finally YAP away
+  auto ciphertext_from_alice = yap::v1::encrypt(shared_secret, bob_keypair->public_key, plaintext);
+  // Flip the first bit of the tag
+  int pos_to_flip = x25519::PUBLIC_KEY_LENGTH + aesgcm::TAG_LENGTH;
+  ciphertext_from_alice[pos_to_flip] = 0x1 ^ ciphertext_from_alice[pos_to_flip];
+  ASSERT_ANY_THROW(yap::v1::decrypt(shared_secret, bob_keypair->private_key, ciphertext_from_alice));
+}
+
+TEST(YAPTests, BadCiphertextHead)
+{
+  // Set up Alice and Bob
+  auto alice_keypair = x25519::generate_keypair();
+  auto bob_keypair = x25519::generate_keypair();
+
+  // Check that the shared secrets are equal
+  auto shared_secret = x25519::derive_secret(alice_keypair->private_key, bob_keypair->public_key);
+
+  // Randomized plaintext that's big and an odd size
+  auto plaintext = encoders::hex_to_binary(commonrand::hex(1050));
+
+  // Finally YAP away
+  auto ciphertext_from_alice = yap::v1::encrypt(shared_secret, bob_keypair->public_key, plaintext);
+  // Flip the first byte of the ciphertext
+  int pos_to_flip = x25519::PUBLIC_KEY_LENGTH + aesgcm::TAG_LENGTH + aesgcm::TAG_LENGTH;
+  ciphertext_from_alice[pos_to_flip] = 0x1 ^ ciphertext_from_alice[pos_to_flip];
+  ASSERT_ANY_THROW(yap::v1::decrypt(shared_secret, bob_keypair->private_key, ciphertext_from_alice));
+}
+
+TEST(YAPTests, BadCiphertextMid)
+{
+  // Set up Alice and Bob
+  auto alice_keypair = x25519::generate_keypair();
+  auto bob_keypair = x25519::generate_keypair();
+
+  // Check that the shared secrets are equal
+  auto shared_secret = x25519::derive_secret(alice_keypair->private_key, bob_keypair->public_key);
+
+  // Randomized plaintext that's big and an odd size
+  auto plaintext = encoders::hex_to_binary(commonrand::hex(1050));
+
+  // Finally YAP away
+  auto ciphertext_from_alice = yap::v1::encrypt(shared_secret, bob_keypair->public_key, plaintext);
+  // Flip somewhere in the middle of the ciphertext
+  int pos_to_flip = 700;
+  ciphertext_from_alice[pos_to_flip] = 0x1 ^ ciphertext_from_alice[pos_to_flip];
+  ASSERT_ANY_THROW(yap::v1::decrypt(shared_secret, bob_keypair->private_key, ciphertext_from_alice));
 }
